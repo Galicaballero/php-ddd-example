80da68ee1640ee84315bc131374c1710e5a30f2f
galicaballero <info@tufuturaweb.com>
2022-10-15 08:24:48 +0200

    Update AnalyticsDomainEventBody.php
    it's the same but simpler. There is no logic to a readonly private value

--- src/Analytics/DomainEvents/Domain/AnalyticsDomainEventBody.php
+++ src/Analytics/DomainEvents/Domain/AnalyticsDomainEventBody.php
@@ -6,12 +6,8 @@ namespace CodelyTv\Analytics\DomainEvents\Domain;
 
 final class AnalyticsDomainEventBody
 {
-    public function __construct(private readonly array $value)
+    public function __construct(public readonly array $value)
     {
     }
 
-    public function value(): array
-    {
-        return $this->value;
-    }
 }
